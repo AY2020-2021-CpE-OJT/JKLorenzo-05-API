007a97bc895bfaf9e367ce5433715cfd13d98bea
Juruel Keanu Lorenzo <39931559+JKLorenzo@users.noreply.github.com>
2021-07-19 14:44:05 +0800

    refactor + encapsulate access token

--- src/modules/AuthManager.ts
+++ src/modules/AuthManager.ts
@@ -5,24 +5,6 @@ import AuthPayload from "../structures/AuthPayload.js";
 
 const _sessions = new Map<string, string>();
 
-function _generateToken(session_id: string): AuthData {
-  // create payload
-  const payload = { pld: { id: session_id } as AuthData } as AuthPayload;
-
-  // generate token
-  const access_token = JWT.sign(payload, process.env.JWT_ACCESS!, {
-    issuer: "JKLorenzoPBAPI",
-    subject: "access",
-    expiresIn: "10m",
-  });
-
-  // add token to sessions
-  _sessions.set(session_id, access_token);
-
-  // return token
-  return { token: access_token };
-}
-
 export default class AuthManager {
   static authenticate(req: Request, res: Response, next: NextFunction) {
     try {
@@ -49,33 +31,54 @@ export default class AuthManager {
     }
   }
 
-  static async register(req: Request, res: Response) {
-    try {
-      const auth_header = req.headers.authorization;
-      const token = auth_header && auth_header.split(" ")[1];
-      const session_id = req.body.id;
-      if (!token) return res.sendStatus(401);
+  static verifyRegisterToken(token: string, data: AuthData) {
+    // verify token
+    const payload = JWT.verify(token, process.env.JWT_REGISTER!, {
+      algorithms: ["HS256"],
+      issuer: "JKLorenzoPBAPP",
+      subject: "register",
+    }) as AuthPayload;
 
-      // verify token
-      const payload = JWT.verify(token, process.env.JWT_REGISTER!, {
-        algorithms: ["HS256"],
-        issuer: "JKLorenzoPBAPP",
-        subject: "register",
-      }) as AuthPayload;
-
-      // check if session id matches the payload
-      if (!payload.pld?.id || payload.pld.id !== session_id) {
-        return res.sendStatus(403);
-      }
-
-      // generate access token
-      const data = _generateToken(session_id);
-
-      // send token
-      await res.json(data);
-    } catch (error) {
-      console.error(error);
-      return res.sendStatus(403);
+    // check if token matches the request body
+    if (!payload.pld || payload.pld.id !== data.id || token !== data.token) {
+      throw new Error("SESSION_MISMATCH");
     }
+  }
+
+  static authenticateSession(data: AuthData): AuthData {
+    // check if session id is invalid
+    if (!data.id) throw new Error("INVALID_SESSION_ID");
+
+    // create payload
+    const payload = { pld: data } as AuthPayload;
+
+    // generate token
+    const access_token = JWT.sign(payload, process.env.JWT_ACCESS!, {
+      issuer: "JKLorenzoPBAPI",
+      subject: "access",
+      expiresIn: "10m",
+    });
+
+    // add token to sessions
+    _sessions.set(data.id, access_token);
+
+    // encapsulate access token
+    const enapsulated_payload = {
+      pld: { id: data.id, token: access_token } as AuthData,
+    } as AuthPayload;
+
+    // encode using register token
+    const encoded_token = JWT.sign(
+      enapsulated_payload,
+      process.env.JWT_REGISTER!,
+      {
+        issuer: "JKLorenzoPBAPI",
+        subject: "registered",
+        expiresIn: "10m",
+      }
+    );
+
+    // return encoded data
+    return { id: data.id, token: encoded_token };
   }
 }
--- src/routes/auth/register/post.ts
+++ src/routes/auth/register/post.ts
@@ -1,7 +1,27 @@
 import { Router } from "express";
 import { MongoClient } from "mongodb";
+import AuthData from "../../../structures/AuthData.js";
 import AuthManager from "../../../modules/AuthManager.js";
 
 export default function (router: Router, client: MongoClient): Router {
-  return router.post("/", AuthManager.register);
+  return router.post("/", (req, res) => {
+    try {
+      const auth_header = req.headers.authorization;
+      const register_token = auth_header && auth_header.split(" ")[1];
+      const register_data = req.body as AuthData;
+      if (!register_token) return res.sendStatus(401);
+
+      // verify register request
+      AuthManager.verifyRegisterToken(register_token, register_data);
+
+      // authenticate session
+      const encoded_data = AuthManager.authenticateSession(register_data);
+
+      // send encoded data
+      res.json(encoded_data);
+    } catch (error) {
+      console.error(error);
+      res.status(403).send(String(error));
+    }
+  });
 }
